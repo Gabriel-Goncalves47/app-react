da4c837ac774e5adcb585e310b8dff6b769fac8e
Gabriel Luiz Gonçalves <blgoncalves@hotmail.com>
2021-11-29 14:05:19 -0300

    Portfólio Responsivo

--- portfolio/src/Paginas/Introducao/Introducao.js
+++ portfolio/src/Paginas/Introducao/Introducao.js
@@ -7,35 +7,31 @@ import labenu from '../../Imagens/labenu.png'
 const Introducao = () => {
     return (
         <div className='introduction-page'>
-            <Header />
+            <Header/>
             <div className="eu">
-                <pre>
+                <span>
                     <b>Gabriel Luiz Gonçalves</b> <br />
-                    Front-End Developer <br />
-                    Logo será um Full Stack Developer <br />
+                    Brasileiro <br />
+                    Mineiro <br />
+                    Jovem <br />
                     Disposto a residir onde a empresa necessitar <br />
                     Preparado para pensar, planejar e executar tarefas para otimizar a produtividade <br />
                     <br />
-                    <b>Minha missão é o seu crescimento empresarial, meu desenvolvimento pessoal e profissional!</b> <br />
-                    <b>Saiba mais sobre mim e meus projetos nas abas Pessoal e galeria deste portfólio e entre em contato comigo!</b>
-                </pre>
+                    <b>Minha missão é o seu crescimento empresarial, meu desenvolvimento pessoal e profissional!</b> <br/>
+                    <b>Saiba mais sobre mim e meus projetos neste portfólio e entre em contato comigo!</b>
+                </span>
             </div>
 
             <div className="tabela">
                 <table>
                     <tr>
                         <th>Atualmente</th>
-                        <td>Labenu <br />
-                            Curso Web Full Stack | Agosto 2021 - Fevereiro 2022
-                        </td>
+                        <td>Labenu</td>
                         <td><img src={labenu} alt="Logo Labenu" /></td>
                     </tr>
                     <tr>
                         <th>Ensino Superior</th>
-                        <td>Universidade Federal de Viçosa <br />
-                            Medicina Veterinária |
-                            2013 - 2018
-                        </td>
+                        <td>Universidade Federal de Viçosa</td>
                         <td><img src={brasao} alt="Brasão UFV" /></td>
                     </tr>
                 </table>
